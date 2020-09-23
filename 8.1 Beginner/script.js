70f3620466d4204347037bf0111a79748e6fbe15
Stephen Scholtz <scholtz.gnome@gmail.com>
2020-09-24 00:47:55 +0200

    test

---
+++ 8.1 Beginner/script.js
@@ -0,0 +1,27 @@
+// 3. Calculate Two People's Individual Ages (7kyu)
+// https://www.codewars.com/kata/58e0bd6a79716b7fcf0013b1
+
+function getAges(sum, difference) {
+  
+}
+
+// console.log(getAges(24, 4)); // should == [14, 10]
+// console.log(getAges(63, -13)); // should == null
+
+// 4. Refactored Greeting (7kyu)
+// https://www.codewars.com/kata/5121303128ef4b495f000001
+
+function greet(myName, yourName) {
+  return "Hello " + yourName + ", my name is " + myName;
+}
+
+// console.log(joe.greet("Kate"));
+// console.log(joe.name);
+
+// 5. JavaScript Class-like Objects
+// https://www.codewars.com/kata/513e1e47c600c93cef000001
+
+// Create your Animal class here
+
+// 6. Return Substring Instance Count (7kyu) 
+// https://www.codewars.com/kata/5168b125faced29f66000005
